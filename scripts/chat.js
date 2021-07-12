356b248ee4991fb70e26ab671dd1bff9c1ec1d24
sreevishnu-ux <sreevishnuvinod@gmail.com>
2021-07-12 20:45:36 +0530

    updated the chat features

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,13 +1,9 @@
-// adding new chat documents
-// setting up a real-time listener to get new chats
-// updating the username
-// updating the room
-
 class Chatroom {
   constructor(room, username){
     this.room = room;
     this.username = username;
     this.chats = db.collection('chats');
+    this.unsub;
   }
   async addChat(message){
     // format a chat object
@@ -23,7 +19,9 @@ class Chatroom {
     return response;
   }
   getChats(callback){
-    this.chats
+    this.unsub = this.chats
+      .where('room', '==', this.room)
+      .orderBy('created_at')
       .onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
           if(change.type === 'added'){
@@ -32,10 +30,14 @@ class Chatroom {
         });
     });
   }
+  updateName(username){
+    this.username = username;
+  }
+  updateRoom(room){
+    this.room = room;
+    console.log('room updated');
+    if(this.unsub){
+      this.unsub();
+    }
+  }
 }
-
-const chatroom = new Chatroom('gaming', 'shaun');
-
-chatroom.getChats(data => {
-  console.log(data);
-});
