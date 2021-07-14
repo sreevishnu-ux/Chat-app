ee72eabc2f96b173d2af9b6243e926c9cb053444
sreevishnu-ux <sreevishnuvinod@gmail.com>
2021-07-14 12:10:39 +0530

    updated usernames

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
-
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg')
 //add a new chat
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -11,9 +12,27 @@ newChatForm.addEventListener('submit', e => {
     .catch(err => console.log("err"))
 
 });
+//update username
+newNameForm.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    //update name via chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    //reset form
+    newNameForm.reset();
+    // show then hide the update messsage
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000 )
+});
+
+//check localstorage for a name
+const username = localStorage.username ? localStorage.username :'anonymous';
+
+
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'Vishnu');
+const chatroom = new Chatroom('gaming', 'username');
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
