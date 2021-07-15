d5a92ca226becf7cbd8184682341abeb350b82b7
sreevishnu-ux <sreevishnuvinod@gmail.com>
2021-07-15 22:43:06 +0530

    final version

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMssg = document.querySelector('.update-mssg')
+const rooms =document.querySelector('.chat-rooms')
 //add a new chat
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -26,13 +27,25 @@ newNameForm.addEventListener('submit', e =>{
     setTimeout(() => updateMssg.innerText = '', 3000 )
 });
 
+//update the chat room
+rooms.addEventListener('click', e =>{
+
+    if(e.target.tagName === 'BUTTON' ){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'))
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+
+
+});
+
 //check localstorage for a name
 const username = localStorage.username ? localStorage.username :'anonymous';
 
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'username');
+const chatroom = new Chatroom('general', 'username');
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
